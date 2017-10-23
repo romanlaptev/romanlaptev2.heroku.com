0470ec9a7515d29cb60ff7d00db2f53dfc346ae5
dev_user <roman-laptev@yandex.ru>
2017-10-23 11:49:43 +0600

    update notes

--- src/main/java/runServer.java
+++ src/main/java/runServer.java
@@ -7,15 +7,15 @@ public class runServer {
 	{
 		Server server = new Server( Integer.valueOf( System.getenv("PORT") ) );
 		WebAppContext ctx = new WebAppContext();
-		ctx.setResourceBase("src/main/webapp");
-		//ctx.setResourceBase("notes");
+		//ctx.setResourceBase("src/main/webapp");
+		ctx.setResourceBase("notes");
 		ctx.setContextPath("/");
 		
-		ctx.setDescriptor("src/main/webapp/WEB-INF/web.xml");
-		//ctx.setDescriptor("notes/WEB-INF/web.xml");
+		//ctx.setDescriptor("src/main/webapp/WEB-INF/web.xml");
+		ctx.setDescriptor("notes/WEB-INF/web.xml");
 
-		ctx.setResourceBase("src/main/webapp");
-		//ctx.setResourceBase("notes");
+		//ctx.setResourceBase("src/main/webapp");
+		ctx.setResourceBase("notes");
 		
 		ctx.setParentLoaderPriority(true);
 		server.setHandler(ctx);
